3cc1790598c1d26869b2edbdfe0c97ddfe2415fc
nzioker <nzioker@gmail.com>
2024-05-08 11:40:01 +0300

    Fixed a lint build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,8 @@ export default function Home() {
               Welcome to the CreativeBlog
             </h1>
             <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-              Here at CreativeBlog, we're all about exploring the possibilities
-              of creativity without bounds. We push boundaries to invent new
-              things and document it for the world to see. Indulge
+              Here at CreativeBlog, {" we're "} all about exploring the
+              possibilities
             </p>
             <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
               <a
