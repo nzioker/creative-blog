1e4aad0c096c96b009dcea9b6b4d4eaf058a8ac3
Ken Nzioka <nzioker@gmail.com>
2024-05-17 16:54:05 +0300

    Added a form to create blogs with.

---
+++ src/app/blog/newBlog/page.tsx
@@ -0,0 +1,42 @@
+import { ClientPageRoot } from "next/dist/client/components/client-page";
+import Image from "next/image";
+import {useState} from 'react';
+
+// function to trigger onclick
+export default function NewBlog() {
+
+    const [title, setTitle] = useState("Title Great")
+    console.log(title)
+  return (
+    <div>
+      <div className="container mx-auto mt-4 ml-4">
+        <form className="max-w-sm mx-auto">
+          <label
+            htmlFor="message"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your title
+          </label>
+          <input
+            type="text"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          ></input>
+          <label
+            htmlFor="message"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-5"
+          >
+            Blog on!
+          </label>
+          <textarea
+            id="message"
+            rows={25}
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Leave a comment..."
+          ></textarea>
+        
+        <button type="button" className="mx-auto mt-3 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,53 +1,62 @@
-import { blogs } from '../../../public/data';
-import Image from 'next/image';
+import { blogs } from "../../../public/data";
+import Image from "next/image";
 
 export default function Blog() {
   return (
-    <div className=" container mx-auto grid grid-cols-3 mt-4 ml-4">
-      {blogs.map((blog, idx) => (
-        <div className="grid grid-col-3 mb-4" key={idx}>
-          <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-              <Image
-                className="object-cover"
-                src="/img/blog1.png"
-                alt="blog-1"
-                width={500}
-                height={500}
-              />
-            </a>
-            <div className="p-5">
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {blog.title}
-                </h5>
-              </a>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {blog.body}
-              </p>
-              <a
-                href="#"
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Read more
-                <svg
-                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10">
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
+    <div>
+      <div className="container mx-auto mt-4 ml-4">
+        
+        
+        
+        <div className="container mx-auto grid grid-cols-3 mt-4 ml-4">
+          {blogs.map((blog, idx) => (
+            <div className="grid grid-col-3 mb-4" key={idx}>
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                  <Image
+                    className="object-cover"
+                    src="/img/blog1.png"
+                    alt="blog-1"
+                    width={500}
+                    height={500}
                   />
-                </svg>
-              </a>
+                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      {blog.title}
+                    </h5>
+                  </a>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {blog.body}
+                  </p>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Read more
+                    <svg
+                      className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 10"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M1 5h12m0 0L9 1m4 4L9 9"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
